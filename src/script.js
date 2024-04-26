de188b47be2021820da5034d11299e064b971bd1
Lucas Bientinezi <lucas.bientinezi@gmail.com>
2024-04-26 17:13:10 -0300

    adicionado todos os arquivos

---
+++ src/script.js
@@ -0,0 +1,25 @@
+// const senha = document.getElementById("senha");
+// const confirmacaoSenha = document.getElementById("confirmacaoSenha");
+// const form = document.getElementById("form");
+// const nome = document.getElementById("nome");
+// const cpf = document.getElementById("cpf");
+// const email = document.getElementById("email");
+// const generoMasculino = document.getElementById("generoMasculino")
+// const generoFeminino = document.getElementById("generoFeminino")
+
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     checkInputNome();
+// })
+
+// function checkInputNome() {
+//     const nomeUsuario = nome.value;
+//     if(nomeUsuario ===""){
+//         // mostrar aviso
+//     }
+// }
+
+// function errorInput(input, message){
+//     const form = input
+
+// }
